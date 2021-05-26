557d1188af6ae9a0b614668ed6906c2c5a703fb7
ajitkumarsingh10@gmail.com <ajitkumarsingh10@gmail.com>
2021-05-26 21:40:52 +0530

    Add project files.

---
+++ CQRS-Microservices/Context/IApplicationContext.cs
@@ -0,0 +1,13 @@
+﻿using CQRS_Microservices.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace CQRS_Microservices.Context
+{
+    public interface IApplicationContext
+    {
+        DbSet<Product> Products { get; set; }
+
+        Task<int> SaveChangesAsync();
+    }
+}
